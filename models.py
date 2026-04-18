7f905560d9d62c69e2d06c7c1114fab5b12937ea
AnnaSam6 <anna.vitaeva@inbox.ru>
2026-04-18 12:39:20 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,48 +6,32 @@ Base = declarative_base()
 
 
 class Character(Base):
-    """Модель персонажа Star Wars с ВСЕМИ обязательными полями"""
+    """Модель персонажа - схема БД"""
     __tablename__ = 'characters'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     character_id = Column(Integer, unique=True, nullable=False, index=True)
     
-    # Основные поля из задания
+    # ОСНОВНЫЕ ПОЛЯ (из задания)
     name = Column(String(200), nullable=False)
     birth_year = Column(String(50))
     eye_color = Column(String(50))
     gender = Column(String(50))
     hair_color = Column(String(50))
-    homeworld = Column(String(200))  # НАЗВАНИЕ планеты, не URL!
+    homeworld = Column(String(200))  # НАЗВАНИЕ планеты!
     mass = Column(Float)
     skin_color = Column(String(50))
     height = Column(Float)
     
-    # Дополнительные поля (связанные списки)
-    films = Column(Text)  # список фильмов через запятую
-    species = Column(Text)  # список видов через запятую
-    starships = Column(Text)  # список кораблей через запятую
-    vehicles = Column(Text)  # список транспорта через запятую
+    # СВЯЗАННЫЕ СПИСКИ (строки через запятую)
+    films = Column(Text)
+    species = Column(Text)
+    starships = Column(Text)
+    vehicles = Column(Text)
     
     # Метаданные
     created_at = Column(DateTime, default=datetime.utcnow)
-    url = Column(String(500))
+    api_url = Column(String(500))
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'character_id': self.character_id,
-            'name': self.name,
-            'birth_year': self.birth_year,
-            'eye_color': self.eye_color,
-            'gender': self.gender,
-            'hair_color': self.hair_color,
-            'homeworld': self.homeworld,
-            'mass': self.mass,
-            'skin_color': self.skin_color,
-            'height': self.height,
-            'films': self.films,
-            'species': self.species,
-            'starships': self.starships,
-            'vehicles': self.vehicles
-        }
+    def __repr__(self):
+        return f"<Character(id={self.character_id}, name='{self.name}', homeworld='{self.homeworld}')>"
